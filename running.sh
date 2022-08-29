fcf0daefded27678ffe7fa4f22c31a093d1986c1
anggradiantoro <110882253+anggradiantoro@users.noreply.github.com>
2022-08-29 18:53:32 +0700

    Update running.sh

--- running.sh
+++ running.sh
@@ -3,50 +3,21 @@ dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Dat
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 #########################
 
-BURIQ () {
-    curl -sS https://raw.githubusercontent.com/scvpnme/permission/main/ipmini > /root/tmp
-    data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
-    for user in "${data[@]}"
-    do
-    exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
-    d1=(`date -d "$exp" +%s`)
-    d2=(`date -d "$biji" +%s`)
-    exp2=$(( (d1 - d2) / 86400 ))
-    if [[ "$exp2" -le "0" ]]; then
-    echo $user > /etc/.$user.ini
-    else
-    rm -f /etc/.$user.ini > /dev/null 2>&1
-    fi
-    done
-    rm -f /root/tmp
-}
-
-MYIP=$(curl -sS ipv4.icanhazip.com)
-Name=$(curl -sS https://raw.githubusercontent.com/scvpnme/permission/main/ipmini | grep $MYIP | awk '{print $2}')
-echo $Name > /usr/local/etc/.$Name.ini
-CekOne=$(cat /usr/local/etc/.$Name.ini)
-
-Bloman () {
-if [ -f "/etc/.$Name.ini" ]; then
-CekTwo=$(cat /etc/.$Name.ini)
-    if [ "$CekOne" = "$CekTwo" ]; then
-        res="Expired"
-    fi
+#license
+echo -e "============================================="
+echo -e " ${GREEN} SCRIPT LICENSE${NC}"
+echo -e "============================================="
+sleep 2
+read -p "SILA MASUKKAN LESEN SCRIPT: " pwd
+if test $pwd == "jinggo007"; then
+echo "Password Accepted!"
 else
-res="Permission Accepted..."
+echo "Password Incorrect!"
+rm -f setup.sh
+sleep 2
+exit
 fi
-}
-
-PERMISSION () {
-    MYIP=$(curl -sS ipv4.icanhazip.com)
-    IZIN=$(curl -sS https://raw.githubusercontent.com/scvpnme/permission/main/ipmini | awk '{print $4}' | grep $MYIP)
-    if [ "$MYIP" = "$IZIN" ]; then
-    Bloman
-    else
-    res="Permission Denied!"
-    fi
-    BURIQ
-}
+clear
 
 x="ok"
 
@@ -59,16 +30,6 @@ rekk='V2RAY'
 bec='v2ray'
 fi
 
-PERMISSION
-
-if [ "$res" = "Expired" ]; then
-Exp="\e[36mExpired\033[0m"
-rm -f /home/needupdate > /dev/null 2>&1
-else
-Exp=$(curl -sS https://raw.githubusercontent.com/scvpnme/permission/main/ipmini | grep $MYIP | awk '{print $3}')
-fi
-clear
-MYIP=$(curl -sS ipv4.icanhazip.com)
 #########################
 # GETTING OS INFORMATION
 source /etc/os-release
